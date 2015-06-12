79470b755cc7d6fdf1cf62bdc9126fbfd9dab3ec
Mollie Holmes <mollie.e.holmes@gmail.com>
2015-06-12 05:18:55 -0700

    Final Form

--- WebInterface.v2/src/controls/createconn1.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Create connection with database.
-$mysqli = new mysqli($host, 
-                    $username, 
-                    $password, 
-                    $database
-);
-?>
--- WebInterface.v2/src/controls/createconn2.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Create connection with database.
-$mysqli2 = new mysqli($host, 
-                    $username, 
-                    $password, 
-                    $database
-);
-?>
